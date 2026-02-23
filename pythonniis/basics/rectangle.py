e7ed440f79b0d43e2fee66206e77ba0a81688041
Your Name <anishmohanty11@gmail.com>
2026-02-23 12:26:06 +0530

    full stack python program

---
+++ pythonniis/basics/rectangle.py
@@ -0,0 +1,12 @@
+#WAP take rectangle length and breadth from kewboard display lentgh,breadth,area and parameter....
+
+print("enter length")
+L= int(input())
+print("enter breadth")
+B=int(input())
+ar=L*B
+pr=2*(L+B)
+print("length=",L)
+print("breadth=",B)
+print("area=",ar)
+print("parameter=",pr)
---
+++ pythonniis/basics/square.py
@@ -0,0 +1,7 @@
+print("enter the length")
+L=int(input())
+ar=L*L
+pr=L*4
+print("length=",L)
+print("Area=",ar)
+print("parameter=",pr)
